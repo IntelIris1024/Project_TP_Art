aa86bbdf3918308a7a3da6b3a02bb408aa58f960
Vasil Grancharov <vasil.grancharov1996@gmail.com>
2017-02-27 18:51:36 +0100

    update

---
+++ Level Design/Unity/3rd_Person/Assets/Interactables.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactables : MonoBehaviour
+{
+
+    public Rect windowRect = new Rect(20, 20, 120, 50);
+    public string stringToEdit = "";
+
+    public Image icon;
+    private bool asshole = false;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Pause();
+            asshole = true;
+            DestroyObject(icon);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            asshole = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (asshole == true) {
+            windowRect = GUILayout.Window(0, windowRect, DoMyWindow, "New Bio!");
+        }
+        }
+    void DoMyWindow(int windowID)
+    {
+        stringToEdit = GUILayout.TextArea(stringToEdit, 400);
+
+    }
+    public static void Pause()
+    {
+        Console.Write("Press any key to continue . . . ");
+        Console.Read();
+    }
+}
